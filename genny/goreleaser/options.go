c7942132410706c061e622872d4b67a7d0e0c16a
Mark Bates <mark@markbates.com>
2018-09-05 11:54:34 -0400

    sorted out the goreleaser template

--- genny/goreleaser/options.go
+++ genny/goreleaser/options.go
@@ -1,16 +1,33 @@
 package goreleaser
 
-import "errors"
+import (
+	"os/user"
+
+	"github.com/pkg/errors"
+)
 
 type Options struct {
-	Force    bool
-	MainFile string
+	Force     bool
+	MainFile  string
+	BrewOwner string
+	BrewTap   string
 }
 
 // Validate that options are usuable
 func (opts *Options) Validate() error {
 	if len(opts.MainFile) == 0 {
 		return errors.New("goreleaser is only for binary applications")
+	}
+	if len(opts.BrewTap) == 0 {
+		opts.BrewTap = "homebrew-tap"
+	}
+
+	if len(opts.BrewOwner) == 0 {
+		user, err := user.Current()
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		opts.BrewOwner = user.Username
 	}
 	return nil
 }
